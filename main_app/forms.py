eeaf5925223e4c360207fe6995ac5423cf59d8ff
losdio <alzabintareq1@gmai.com>
2025-10-14 10:31:25 +0300

    Refactor SignupForm to ensure password is hashed upon saving user instance

--- main_app/forms.py
+++ main_app/forms.py
@@ -22,3 +22,10 @@ class SignupForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'address', 'phone_number']
+        
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
