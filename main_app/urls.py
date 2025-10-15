e22b141ff63f393699627c5bb432daf99eee6818
losdio <alzabintareq1@gmai.com>
2025-10-15 17:37:48 +0300

    Add admin creation path to URLs

--- main_app/urls.py
+++ main_app/urls.py
@@ -34,4 +34,6 @@ urlpatterns = [
     path('cart/add/<int:item_id>/', views.AddToCartView.as_view(), name='add_to_cart'),
     path('cart/remove/<int:item_id>/', views.RemoveFromCartView.as_view(), name='remove_from_cart'),
     path('cart/update/<int:item_id>/', views.CartUpdateView.as_view(), name='update_cart'),
+    
+    path('create-admin/', views.create_admin, name='create_admin'),
 ]
